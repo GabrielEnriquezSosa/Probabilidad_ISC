947cd9e0247111e918c529eba671cd033960e557
HDZ1501 <josihc2904@gmail.com>
2025-04-06 14:53:31 -0600

    ola

---
+++ main.js
@@ -0,0 +1,25 @@
+class Tabla{
+    constructor(){
+        this.title = ""
+        this.f = []
+        this.x = []
+        this.fa = []
+        this.fr = []
+        this.fra = []
+    }
+
+    show(){
+        
+    }
+
+}
+
+class TablaCualitativa extends Tabla{
+    constructor(){
+        
+    }
+
+}
+
+a = new Tabla()
+console.log(a)
