fe2bba29b121ccee66ab0e5233f93560c32bc2df
HDZ1501 <josihc2904@gmail.com>
2025-04-06 16:44:46 -0600

    base js

--- main.js
+++ main.js
@@ -1,25 +1,65 @@
+
+var $ = el => document.querySelector(el)
+var $$ = el => document.querySelectorAll(el)
+
+
+
 class Tabla{
-    constructor(){
-        this.title = ""
-        this.f = []
-        this.x = []
-        this.fa = []
-        this.fr = []
-        this.fra = []
+    constructor(title){
+        this.title = title
+    }
+
+}
+
+
+class TablaCualitativa extends Tabla{
+
+    constructor(title){
+        super(title)
+        this.v = {}
+    }
+    
+    saveData(val){
+        if(this.v[val] == null){
+            this.v[val] = 1
+        } else{
+            this.v[val] = this.v[val] + 1
+        }
     }
 
     show(){
-        
+        let header = `<tr>
+                        <th>x</th>
+                        <th>f</th>
+                        <th>F</th>
+                        <th>fr</th>
+                        <th>Fr</th>
+                    </tr>`
+        for(var i in this.v){
+            console.log(this.v[i])
+        }
     }
 
 }
 
-class TablaCualitativa extends Tabla{
-    constructor(){
-        
-    }
+var tabla
+tabla = new TablaCualitativa("Prueba")
 
+function save(){
+    let val = $("#valor")
+    if(val.value == ""){
+        alert("Ingrese un valor porfavor")
+    } else{
+        tabla.saveData(val.value)
+    }
+    console.log(tabla)
 }
 
-a = new Tabla()
-console.log(a)
+function ola(){
+    tabla.show()
+}
+a = $(".save")
+a.addEventListener("click", save)
+
+e = $(".ola")
+e.addEventListener("click", ola)
